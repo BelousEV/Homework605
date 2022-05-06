0b0f1bdadb74a5d1b0f7399f375ccad140b8b17d
ElenaR <llinusik@mail.ru>
2022-05-06 17:18:07 +0300

    Готовое ДЗ. Доступно 3 задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задание 1
+
+        String firstName = "Семён ";
+        String  middleName = "Семёнович";
+        String lastName = "Иванов ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println(fullName);
+
+        // Задание 2
+
+
+        fullName = fullName.toUpperCase();
+        System.out.println(fullName);
+
+        // Задание 3
+
+
+
+        fullName = fullName.replace('Ё','Е');
+        System.out.println( "Данные ФИО сотрудника — " + fullName);
+
+
     }
 }
